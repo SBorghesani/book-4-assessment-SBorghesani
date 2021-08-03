a8ed1d1da16c42f74e76a2d339344bbaf2eaec44
Stefan Borghesani <stefan.borghesani@gmail.com>
2021-08-03 14:20:17 -0500

    book 4 assessment - fixed bugs and displayed relevant information

--- scripts/SideDishes.js
+++ scripts/SideDishes.js
@@ -1,8 +1,10 @@
-import { getSides, setSide } from "./database.js"
+import { getSides, setSide, getComboChoices } from "./database.js"
 
 const sideDishes = getSides()
 
-document.addEventListener("change", (event) => {
+document.addEventListener(
+    "change", 
+    (event) => {
     if (event.target.name === "sideDish") {
         setSide(parseInt(event.target.value))
     }
@@ -10,12 +12,19 @@ document.addEventListener("change", (event) => {
 
 // Requirement: The radio input elements that this component funcion renders must all have a name of "sideDish"
 export const Sides = () => {
+    const comboChoices = getComboChoices()
     let html = "<ul>"
 
-    const listItems = sideDishes.map(dish => {
+    const listItems = sideDishes.map(side => {
+        if (side.id === comboChoices.sideId) {
         return `<li>
-            <input type="radio" />
+            <input type="radio" name="sideDish" value="${side.id}" required checked /> ${side.title}
         </li>`
+        } else {
+            return `<li>
+            <input type="radio" name="sideDish" value="${side.id}" required /> ${side.title}
+        </li>`
+        }
     })
 
     html += listItems.join("")
